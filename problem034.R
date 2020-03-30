13eff032c99cb512b4daa84378d782c11a733600
Peter Prevos <peter@prevos.net>
2020-03-30 15:54:47 +1100

    Restructured

--- problem034.R
+++
@@ -1,9 +0,0 @@
-# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
-
-answer <- 0
-for (n in 3:999999) {
-    digs <- as.numeric(unlist(strsplit(as.character(n), "")))
-    if (sum(factorial(digs)) == n) answer <- answer + n
-}
-print(answer)
-
